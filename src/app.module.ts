f310e70f271809179b60bfd5b7c0bd5801ec0854
Ben <ben.a.burgos@gmail.com>
2022-08-10 16:51:27 -0700

    creates db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TicketsModule } from './tickets/tickets.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [TicketsModule],
+  imports: [
+    TicketsModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://SEI:ePQZjyECSisqCJZj@sei.4ivtg.mongodb.net/pandaPad?retryWrites=true&w=majority',
+      { useNewUrlParser: true },
+    ),
+  ],
   controllers: [],
   providers: [],
 })
