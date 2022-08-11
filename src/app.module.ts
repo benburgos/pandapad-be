b2e8b0c0eba3ef199a119fea06bb25483a0f7493
Ben <ben.a.burgos@gmail.com>
2022-08-11 15:48:23 -0700

    refactoring twilio use case

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { TicketsModule } from './tickets/tickets.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthzModule } from './authz/authz.module';
-import ConversationsController from './conversations/conversations.controller';
-import TwilioService from './twilio/twilio.service';
-import DialogflowService from './dialogflow/dialogflow.service';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
     TicketsModule,
     MongooseModule.forRoot(
       'mongodb+srv://SEI:ePQZjyECSisqCJZj@sei.4ivtg.mongodb.net/pandaPad?retryWrites=true&w=majority',
@@ -17,7 +12,7 @@ import DialogflowService from './dialogflow/dialogflow.service';
     ),
     AuthzModule,
   ],
-  controllers: [ConversationsController],
-  providers: [TwilioService, DialogflowService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
