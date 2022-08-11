79969c5d945bd9afe700b7d4c2ca67d687dc879f
Ben <ben.a.burgos@gmail.com>
2022-08-11 15:11:00 -0700

    configures dialogflow service

--- src/dialogflow/dialogflow.service.ts
+++ src/dialogflow/dialogflow.service.ts
@@ -1,4 +1,56 @@
+import dialogflow, { SessionsClient } from '@google-cloud/dialogflow';
+import { google } from '@google-cloud/dialogflow/build/protos/protos';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { v4 as uuid } from 'uuid';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Injectable()
-export class DialogflowService {}
+export default class DialogflowService {
+  private readonly sessionClient: SessionsClient;
+  private readonly sessionPath: string;
+
+  constructor(private configService: ConfigService) {
+    const sessionId = uuid();
+    const dialogflowProjectId = this.configService.get(
+      process.env.GCP_DF_PROJECT_ID,
+    );
+    const dialogFlowPrivateKey = this.configService.get(
+      process.env.GCP_DF_PRIVATE_KEY,
+    );
+    const dialogFlowClientEmail = this.configService.get(
+      process.env.GCP_DF_CLIENT_EMAIL,
+    );
+
+    this.sessionClient = new dialogflow.SessionsClient({
+      credentials: {
+        private_key: dialogFlowPrivateKey,
+        client_email: dialogFlowClientEmail,
+      },
+    });
+
+    this.sessionPath = this.sessionClient.projectAgentSessionPath(
+      dialogflowProjectId,
+      sessionId,
+    );
+  }
+
+  public getResponse = async (userMessage: string) => {
+    const request: google.cloud.dialogflow.v2.IDetectIntentRequest = {
+      session: this.sessionPath,
+      queryInput: {
+        text: {
+          text: userMessage,
+          languageCode: 'en-US',
+        },
+      },
+    };
+    const [response] = await this.sessionClient.detectIntent(request);
+    const textResponse =
+      response.queryResult?.fulfillmentText ||
+      'Can you please ask me something else?';
+    return textResponse;
+  };
+}
