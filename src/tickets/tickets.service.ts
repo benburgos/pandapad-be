7c5861a1ac2cb8599e49cf2fe640502c0af42029
Ben <ben.a.burgos@gmail.com>
2022-08-10 17:19:08 -0700

    completes init service

--- src/tickets/tickets.service.ts
+++ src/tickets/tickets.service.ts
@@ -1,4 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { Ticket } from './interfaces/ticket.interface';
+import { CreateTicketDTO } from './dto/create-ticket.dto';
 
 @Injectable()
-export class TicketsService {}
+export class TicketsService {
+  constructor(
+    @InjectModel('Ticket') private readonly ticketModel: Model<Ticket>,
+  ) {}
+
+  async createTicket(createTicketDTO: CreateTicketDTO): Promise<Ticket> {
+    const newTicket = await new this.ticketModel(createTicketDTO);
+    return newTicket.save();
+  }
+
+  async getTicket(ticketId): Promise<Ticket> {
+    const ticket = await this.ticketModel.findById(ticketId).exec();
+    return ticket;
+  }
+
+  async getTickets(): Promise<Ticket[]> {
+    const tickets = await this.ticketModel.find().exec();
+    return tickets;
+  }
+
+  async editTicket(
+    ticketId,
+    createTicketDTO: CreateTicketDTO,
+  ): Promise<Ticket> {
+    const editedTicket = await this.ticketModel.findByIdAndUpdate(
+      ticketId,
+      createTicketDTO,
+      { new: true },
+    );
+    return editedTicket;
+  }
+
+  async deleteTicket(ticketId): Promise<any> {
+    const deletedTicket = await this.ticketModel.findByIdAndRemove(ticketId);
+    return deletedTicket;
+  }
+}
