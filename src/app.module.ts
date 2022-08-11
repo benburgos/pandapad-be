52121cbc427721a6668cce1f926a32183cf8e052
Ben <ben.a.burgos@gmail.com>
2022-08-10 18:07:06 -0700

    adds auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TicketsModule } from './tickets/tickets.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthzModule } from './authz/authz.module';
 
 @Module({
   imports: [
@@ -9,6 +10,7 @@ import { MongooseModule } from '@nestjs/mongoose';
       'mongodb+srv://SEI:ePQZjyECSisqCJZj@sei.4ivtg.mongodb.net/pandaPad?retryWrites=true&w=majority',
       { useNewUrlParser: true },
     ),
+    AuthzModule,
   ],
   controllers: [],
   providers: [],
