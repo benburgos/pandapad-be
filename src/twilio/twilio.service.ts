e9b254430ce9f982f26f2fbfb729671d84fcf11c
Ben <ben.a.burgos@gmail.com>
2022-08-11 15:08:19 -0700

    adds twilio service

--- src/twilio/twilio.service.ts
+++ src/twilio/twilio.service.ts
@@ -1,4 +1,73 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Twilio } from 'twilio';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Injectable()
-export class TwilioService {}
+export default class TwilioService {
+  private readonly client: Twilio;
+  private readonly conversationsServiceId: string;
+
+  constructor(private configService: ConfigService) {
+    this.client = new Twilio(
+      this.configService.get(process.env.SERVICE_TWILIO_KEY_SID),
+      this.configService.get(process.env.SERVICE_TWILIO_KEY_SECRET),
+      {
+        accountSid: this.configService.get(
+          process.env.SERVICE_TWILIO_ACCOUNT_SID,
+        ),
+      },
+    );
+    this.conversationsServiceId = this.configService.get(
+      process.env.SERVICE_TWILIO_CONVERSATION_SID,
+    );
+  }
+
+  /**
+   * Creates a scoped conversation webhook
+   * @param conversationId
+   * @returns webhook sid
+   */
+  public createConversationWebhook = async (
+    conversationId: string,
+  ): Promise<string> => {
+    const conversationsUrl = `${this.configService.get(
+      process.env.APPLICATION_ENDPOINT,
+    )}/conversations/${conversationId}/events`;
+
+    const createRequest = await this.client.conversations
+      .conversations(conversationId)
+      .webhooks.create({
+        configuration: {
+          method: 'POST',
+          filters: ['onMessageAdded'],
+          url: conversationsUrl,
+          replayAfter: 0,
+        },
+        target: 'webhook',
+      });
+
+    return createRequest.sid;
+  };
+
+  /**
+   * Creates a message in the conversations service
+   * @param conversationId
+   * @param message
+   * @returns message sid
+   */
+  public sendMessage = async (conversationId: string, message: string) => {
+    const messageResponse = await this.client.conversations
+      .services(this.conversationsServiceId)
+      .conversations(conversationId)
+      .messages.create({
+        body: message,
+      });
+
+    return messageResponse.sid;
+  };
+
+  public getClient = (): Twilio => this.client;
+}
