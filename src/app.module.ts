d6faabe42f8d2963a85494884343617ccbc12fff
Ben <ben.a.burgos@gmail.com>
2022-08-11 15:18:20 -0700

    imports controller&services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TicketsModule } from './tickets/tickets.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthzModule } from './authz/authz.module';
-import { TwilioService } from './twilio/twilio.service';
-import { DialogflowService } from './dialogflow/dialogflow.service';
-import { ConversationsController } from './conversations/conversations.controller';
+import ConversationsController from './conversations/conversations.controller';
+import TwilioService from './twilio/twilio.service';
+import DialogflowService from './dialogflow/dialogflow.service';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TicketsModule,
     MongooseModule.forRoot(
       'mongodb+srv://SEI:ePQZjyECSisqCJZj@sei.4ivtg.mongodb.net/pandaPad?retryWrites=true&w=majority',
