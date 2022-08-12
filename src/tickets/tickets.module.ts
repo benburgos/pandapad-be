1542a1bb3d22d895328887e7741a28a7f6dbc761
Ben <ben.a.burgos@gmail.com>
2022-08-11 21:47:15 -0700

    removes twilio

--- src/tickets/tickets.module.ts
+++ src/tickets/tickets.module.ts
@@ -3,15 +3,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { TicketSchema } from './schemas/ticket';
 import { TicketController } from './tickets.controller';
 import { TicketService } from './tickets.service';
-import { TwilioModule } from 'nestjs-twilio';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'Ticket', schema: TicketSchema }]),
-    TwilioModule.forRoot({
-      accountSid: process.env.TWILIO_ACCOUNT_SID,
-      authToken: process.env.TWILIO_AUTH_TOKEN,
-    }),
   ],
   providers: [TicketService],
   controllers: [TicketController],
