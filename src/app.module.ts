bf8ec6f97b87e10d8108419ef8b97b70dce0f62c
Ben <ben.a.burgos@gmail.com>
2022-08-14 21:56:54 -0700

    adds env for db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,16 @@ import { Module } from '@nestjs/common';
 import { TicketsModule } from './tickets/tickets.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthzModule } from './authz/authz.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
   imports: [
     TicketsModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://SEI:ePQZjyECSisqCJZj@sei.4ivtg.mongodb.net/pandaPad?retryWrites=true&w=majority',
-      { useNewUrlParser: true },
-    ),
+    MongooseModule.forRoot(`${process.env.DATABASE_URL}`, {
+      useNewUrlParser: true,
+    }),
     AuthzModule,
   ],
   controllers: [],
